0339ff979ae67e042c2ebca1ffdcbd2ea3444c37
Stephen Sparling <stephen@finnovate.io>
2025-04-25 16:11:10 -0400

    add blog list page data

---
+++ src/api/page-blog-list/controllers/page-blog-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-blog-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-blog-list.page-blog-list');
---
+++ src/api/page-blog-list/services/page-blog-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-blog-list service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::page-blog-list.page-blog-list');
